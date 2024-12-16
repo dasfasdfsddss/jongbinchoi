37d2c66cc35aede7d40dee8890a54dbf218ccb00
jongbinchoi <jongbin985@naver.com>
2024-12-16 16:40:16 +0900

    Add : README.md

---
+++ src/main/java/com/gitdemo/jbchoi/JbchoiApplication.java
@@ -0,0 +1,13 @@
+package com.gitdemo.jbchoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbchoiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JbchoiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gitdemo/jbchoi/JbchoiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitdemo.jbchoi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JbchoiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
